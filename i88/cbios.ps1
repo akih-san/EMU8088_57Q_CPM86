88aa6e14be5a8ad7798469c84d965f681eec9e51
@Akih_san <112370246+akih-san@users.noreply.github.com>
2024-01-24 22:07:03 +0900

    Add files via upload

--- i88/cbios.ps1
+++ i88/cbios.ps1
@@ -1,2 +1,2 @@
 Get-Content cbios_*.bin -Encoding Byte | Set-Content cbios.bin -Encoding Byte
-.\bin2inc.exe .\cbios.bin | sc cbios.inc
+.\mkinc.exe .\cbios.bin | sc cbios.inc
--- i88/unimon_8086.ps1
+++ i88/unimon_8086.ps1
@@ -1,1 +1,1 @@
-.\bin2inc.exe .\unimon_8086.bin | sc unimon_8086.inc
+.\mkinc.exe .\unimon_8086.bin | sc unimon_8086.inc
